e674266434221a1a1efdc19200c2909e6032be9b
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2025-05-26 13:39:40 +0530

    changes done

--- app/api/auth/skiilsmatching/route.ts
+++ app/api/auth/skiilsmatching/route.ts
@@ -1,6 +1,5 @@
 export const dynamic = 'force-dynamic';
 
-
 import { NextRequest, NextResponse } from 'next/server';
 import { getClient } from '../../../../lib/db';
 import { getServerSession } from 'next-auth/next';
