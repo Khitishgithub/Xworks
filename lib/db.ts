a265acdbcec84e88899085c6ee2d5f7c5f6d4d90
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2025-05-26 13:36:13 +0530

    changes done

--- lib/db.ts
+++ lib/db.ts
@@ -1,19 +1,17 @@
-
 import { Pool, Client } from 'pg';
 
-// Define the database configuration
 const pool = new Pool({
-  user: 'postgres',  
-  host: 'localhost',     
-  database: 'postgres',  
-  password: 'postgres', 
-  port: 5432,  
+  user: 'postgres',
+  host: 'shortline.proxy.rlwy.net', 
+  database: 'railway',
+  password: 'NODWKQYSeqooyCKMROkiCcUlnNHwlySk',
+  port: 59816, 
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
-
 
 export const getClient = async (): Promise<Client> => {
   const client = await pool.connect();
   return client;
 };
-
-
